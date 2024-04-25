ace9674c577d3f259e2080d9349b8e5bf2152de8
Sheremeta <vander.cold@mail.ru>
2024-04-25 15:32:49 +0700

    Изменили поведение программы при наличии польз. параметров в комм. строке

--- args.c
+++ args.c
@@ -3,14 +3,18 @@
 #include <sys/types.h>
 #include <unistd.h>
 int main(int argc, char *argv[], char *envp[]) {
-	printf("Кол-во параметров: %d\n", argc); 
-	for(int i=0; i<argc; i++) {
-		printf("Параметр %d: %s\n", i, argv[i]);
+	printf("Кол-во параметров: %d\n", argc);
+	if (argc>1) { 
+		for(int i=0; i<argc; i++) {
+			printf("Параметр %d: %s\n", i, argv[i]);
+		}
 	}
-	int j=0;
-	while (envp[j]!=NULL) {
-		printf("Параметр окружения %d, %s\n", j, envp[j]);
-		j++;
+	else {
+		int j=0;
+			while (envp[j]!=NULL) {
+			printf("Параметр окружения %d, %s\n", j, envp[j]);
+			j++;
+		}
 	}
 	return 0;
 }
